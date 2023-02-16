8758128147abba31312cb1f8656ac43ee86b9752
Daryl-YANG <41143480+Daryl-YANG@users.noreply.github.com>
2023-02-15 19:37:29 -0500

    update Feb 15 2023

--- code/lidar_parrelle_process_chm.R
+++ code/lidar_parrelle_process_chm.R
@@ -14,7 +14,7 @@ dlm <- .Platform$file.sep # <--- What is the platform specific delimiter?
 
 #****************************** load required libraries **********************************#
 ### install and load required R packages
-list.of.packages <- c("lidR", 'ggplot2', 'future', 'rgdal')  
+list.of.packages <- c("lidR", 'ggplot2', 'future', 'rgdal', 'terra')  
 # check for dependencies and install if needed
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies=c("Depends", "Imports",
@@ -102,7 +102,6 @@ if (classify == 'YES')
 ### create digital terrain model raster ile
 opt_output_files(classified_ctg) <-  paste0(temp.dir, "/{*}_dtm")
 opt_stop_early(classified_ctg) <- FALSE
-opt_merge(ctg_norm) <- TRUE
 dtm <- rasterize_terrain(classified_ctg, reso, tin())
 
 ### create canopy height laz file
@@ -117,74 +116,14 @@ opt_merge(ctg_norm) <- TRUE
 chm <- rasterize_canopy(ctg_norm, reso, p2r(0.15), overwrite=TRUE)
 
 dtm.filename <- paste0(out.dir, '/', 'digital_terrain_model.tif')
-writeRaster(dtm, dtm.filename, format = 'GTiff', overwrite = TRUE)
+writeRaster(dtm, dtm.filename, overwrite = TRUE)
 
 chm.filename <- paste0(out.dir, '/', 'canopy_height_model.tif')
-writeRaster(chm, chm.filename, format = 'GTiff', overwrite = TRUE)
+writeRaster(chm, chm.filename, overwrite = TRUE)
 
-
-
-
-
-
-
-
-### create canopy height laz file
-opt_output_files(classified_ctg) <-  paste0(temp.dir, "\\{*}_norm")
-#opt_merge(classified_ctg) <- TRUE
-opt_stop_early(classified_ctg) <- FALSE
-ctg_norm <- normalize_height(classified_ctg, dtm)
-#opt_merge(ctg) <- TRUE
-
-linear_stretch <- function(las) 
-{
-  las$Z <- las$Z*10
-  return(las)
-}
-new_ctg <- catalog_map(ctg_norm, linear_stretch)
-
-### identify shrubs
-f <- function(x) {x * 0.1 + 3}
-heights <- seq(0,5,1)
-ws <- f(heights)
-#opt_output_files(new_ctg) <- ""
-#ttops <- locate_trees(new_ctg, lmf(f))
-opt_output_files(new_ctg) <- "" #paste0(out.dir, "/{*}_treeid")
-#opt_merge(new_ctg) <- TRUE
-ttops <- locate_trees(new_ctg, lmf(f), uniqueness = "bitmerge")
-#ttops$treeID <- 1:nrow(ttops)
-ttops_merge <- do.call(rbind, ttops)
-outname <- paste0(out.dir, '/', 'flight5_tree_id.shp')
-#writeOGR(ttops_merge, outname, driver="ESRI Shapefile")
-st_write(ttops_merge, outname, driver = "ESRI Shapefile")
-
-### segment individual shrubs
-opt_output_files(new_ctg) <- paste0(temp.dir, "/chm_{*}")
-opt_merge(new_ctg) <- TRUE
-opt_stop_early(new_ctg) <- FALSE
-chm <- rasterize_canopy(new_ctg, 1, p2r(0.15), overwrite=TRUE)
-
-opt_output_files(new_ctg) <- paste0(temp.dir, "/{*}_segmented")
-algo <- dalponte2016(chm, ttops_merge)
-opt_merge(new_ctg) <- TRUE
-opt_stop_early(new_ctg) <- FALSE
-ctg_segmented <- segment_trees(new_ctg, algo)
-#outname <- paste0(data.dir, '/', 'tree_segment.shp')
-#writeOGR(ctg_segmented, outname, driver = "ESRI Shapefile")
-#algo <- dalponte2016(chm, ttops)
-#ctg_segmented <- catalog_map(new_ctg, segment_trees(algo))
-
-#ctg_segmented <- segment_trees(ctg_norm, algo)
-outname <- paste0(out.dir, '/', 'flight5_tree_seg')
-opt_output_files(ctg_segmented) <- outname
-opt_chunk_buffer(ctg_segmented) <- 0
-opt_chunk_size(ctg_segmented) <- 10000
-singlefile_ctg <- catalog_retile(ctg_segmented)
-
-temp <- crown_metrics(singlefile_ctg, func = .stdtreemetrics, geom = "convex")
-
+### remove the temp folder
 unlink(temp.dir, recursive = T, force = T)
-
+#*****************************************************************************************#
 
 
 
